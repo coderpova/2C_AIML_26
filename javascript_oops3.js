fda68b18d2cff68ca1141cd098e18c73b4d94485
coderpova <namansharma72976@gmail.com>
2026-09-23 14:42:25 +0530

    next commit

---
+++ javascript_oops3.js
@@ -0,0 +1,34 @@
+class Employee {
+    constructor(id, name, basicSalary) {
+        this.id = id;
+        this.name = name;
+        this.basicSalary = basicSalary;
+    }
+
+    calculateSalary() {
+        return this.basicSalary;
+    }
+}
+
+class Manager {
+    constructor(id, name, basicSalary, incentive) {
+        this.id = id;
+        this.name = name;
+        this.basicSalary = basicSalary;
+        this.incentive = incentive;
+    }
+
+    calculateSalary() {
+        return this.basicSalary + this.incentive;
+    }
+}
+
+let emp = new Employee(101, "Rahul", 30000);
+let manager = new Manager(102, "Naman", 50000, 10000);
+
+
+console.log("Employee Name:", emp.name);
+console.log("Employee Salary:", emp.calculateSalary());
+
+console.log("Manager Name:", manager.name);
+console.log("Manager Salary:", manager.calculateSalary());
